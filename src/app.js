b6e0104b7156206c15de2f4fcd8e3f43500e2e59
KanishaLiyanage <kanishaliyanage97@gmail.com>
2022-09-28 09:02:12 +0530

    pages created

--- src/app.js
+++ src/app.js
@@ -6,10 +6,29 @@ app.set('view engine', 'ejs');
 const port = process.env.PORT || 3000;
 
 app.get('/', async (req, res) => {
+    res.render('landing');
+});
+
+app.get('/signUp', async (req, res) => {
+    res.render('signUp');
+});
+
+app.get('/signIn', async (req, res) => {
+    res.render('signIn');
+});
+
+app.get('/home', async (req, res) => {
     res.render('home');
 });
 
+app.get('/profile', async (req, res) => {
+    res.render('profile');
+});
 
-app.listen(port, function(){
+app.get('/compose', async (req, res) => {
+    res.render('compose');
+});
+
+app.listen(port, function () {
     console.log("Server up on port " + port + ".");
 });
