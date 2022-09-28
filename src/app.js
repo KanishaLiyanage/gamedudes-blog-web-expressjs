a9bd192c3a19c5a3e5e9e63cfc7a6a1c0ecf8a18
KanishaLiyanage <kanishaliyanage97@gmail.com>
2022-09-28 10:13:32 +0530

    home page ui

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 const port = process.env.PORT || 3000;
 
 app.get('/', async (req, res) => {
-    res.render('landing');
+    res.render('home');
 });
 
 app.get('/signUp', async (req, res) => {
@@ -17,16 +20,16 @@ app.get('/signIn', async (req, res) => {
     res.render('signIn');
 });
 
-app.get('/home', async (req, res) => {
-    res.render('home');
-});
-
 app.get('/profile', async (req, res) => {
     res.render('profile');
 });
 
 app.get('/compose', async (req, res) => {
     res.render('compose');
+});
+
+app.get('/postID', async (req, res) => {
+    res.render('post');
 });
 
 app.listen(port, function () {
