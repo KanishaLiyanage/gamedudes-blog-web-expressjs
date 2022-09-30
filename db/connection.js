9448f137f4a0f9406c25b98ccb9ad8e919b74053
KanishaLiyanage <kanishaliyanage97@gmail.com>
2022-09-30 14:30:46 +0530

    db url commit

--- db/connection.js
+++ db/connection.js
@@ -5,6 +5,6 @@ const mongoose = require('mongoose');
 const localDB = "mongodb://127.0.0.1:27017/GameDudeDB";
 const atlasURL = process.env.ATLAS_URL;
 
-mongoose.connect(localDB);
+// mongoose.connect(localDB);
 
-// mongoose.connect(atlasURL);
+mongoose.connect(atlasURL);
